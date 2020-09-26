c0f51afadfbfe59269e9a70b0d7a9fd313d9e75c
xroll <xroll.gothub@gmail.com>
2020-09-26 23:12:53 +0200

    (Feat) Clock : - Add clock behaviour in script.js - start clock function - stop clock function - toggle clock function - spacebar event listener - countdown function

--- script.js
+++ script.js
@@ -78,6 +78,13 @@ function initialState(timer){
     let initialHoursLeft = 0;
     let initialMinutesLeft = 0;
     let initialSecondsLeft = 0;
+
+    const whiteHours = document.getElementById('whiteHours');
+    const blackHours = document.getElementById('blackHours');
+    const whiteMinutes = document.getElementById('whiteMinutes');
+    const blackMinutes = document.getElementById('blackMinutes');
+    const whiteSeconds = document.getElementById('whiteSeconds');
+    const blackSeconds = document.getElementById('blackSeconds');
     
     //Initial time
     initialHoursLeft = Math.floor(timer / 3600) % 24;
@@ -89,21 +96,21 @@ function initialState(timer){
     //Hours
     console.log(initialHoursLeft);
     if(initialHoursLeft > 0) {
-        document.getElementById('whiteHours').style.display = "block";
-        document.getElementById('blackHours').style.display = "block";
-        document.getElementById('whiteHours').innerHTML = initialHoursLeft + ' :&nbsp;';
-        document.getElementById('blackHours').innerHTML = initialHoursLeft + " :&nbsp;";
+        whiteHours.style.display = "block";
+        blackHours.style.display = "block";
+        whiteHours.innerHTML = initialHoursLeft + ' :&nbsp;';
+        blackHours.innerHTML = initialHoursLeft + " :&nbsp;";
     }
     else {        
-        document.getElementById('whiteHours').style.display = "none";
-        document.getElementById('blackHours').style.display = "none";
+        whiteHours.style.display = "none";
+        blackHours.style.display = "none";
     }    
     //Minutes    
-    document.getElementById('whiteMinutes').innerHTML = formatTime(initialMinutesLeft) + " :&nbsp;";
-    document.getElementById('blackMinutes').innerHTML = formatTime(initialMinutesLeft) + " :&nbsp;";
+    whiteMinutes.innerHTML = formatTime(initialMinutesLeft) + " :&nbsp;";
+    blackMinutes.innerHTML = formatTime(initialMinutesLeft) + " :&nbsp;";
     //Seconds
-    document.getElementById('whiteSeconds').innerHTML = formatTime(initialSecondsLeft);    
-    document.getElementById('blackSeconds').innerHTML = formatTime(initialSecondsLeft);
+    whiteSeconds.innerHTML = formatTime(initialSecondsLeft);    
+    blackSeconds.innerHTML = formatTime(initialSecondsLeft);
 
     //Set initial time for players
 
@@ -120,4 +127,92 @@ function initialState(timer){
 //Format time
 function formatTime(time) {
     return time < 10 ? (`0${time}`) : time;
+}
+
+function startClock(){
+    isWhiteActive = true;
+    isClockActive = true;
+    clock();
+}
+
+function clock(){
+    clockTick = setInterval(countdown, 1000);
+ }
+
+ function stopClock(){
+    clearInterval(clockTick);
+}
+
+ function countdown() {
+
+    if(isWhiteActive){
+
+        if(whiteTimeLeft === 0)
+        {
+            stopClock();
+        }
+        else{
+            whiteTimeLeft = whiteTimeLeft -1;
+
+            let whiteHoursLeft = Math.floor(whiteTimeLeft / 3600) % 24;
+            let whiteMinutesLeft = Math.floor(whiteTimeLeft / 60) % 60;
+            let whiteSecondsLeft = whiteTimeLeft % 60;
+    
+            whiteHours.innerHTML = whiteHoursLeft + " : ";
+            whiteMinutes.innerHTML = formatTime(whiteMinutesLeft) + " : ";
+            whiteSeconds.innerHTML = formatTime(whiteSecondsLeft);
+        }
+    }
+    else if (isBlackActive){
+
+        if(blackTimeLeft === 0)
+        {
+            stopClock();
+        }
+        else{
+            blackTimeLeft = blackTimeLeft -1 ; 
+
+            let blackHoursLeft = Math.floor(blackTimeLeft / 3600) % 24;
+            let blackMinutesLeft = Math.floor(blackTimeLeft / 60) % 60;
+            let blackSecondsLeft = blackTimeLeft % 60;
+            
+            blackHours.innerHTML = blackHoursLeft + " : ";
+            blackMinutes.innerHTML = formatTime(blackMinutesLeft) + " :      ";
+            blackSeconds.innerHTML = formatTime(blackSecondsLeft);
+        }
+    }    
+}
+
+//Toggle active player
+
+function toggleClock(){
+    isWhiteActive = !isWhiteActive;
+    isBlackActive = !isBlackActive;
+}
+
+//Toggle by timer press
+function whiteToggle(){
+    if(isWhiteActive){
+        toggleClock()
+    }
+}
+
+function blackToggle(){
+    if(isBlackActive){
+        toggleClock()
+    }
+}
+
+//Toggle with Spacebar input listener
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        console.log(isWhiteActive, isBlackActive);
+        if(!isClockActive){
+            isClockActive = true;
+            startClock();
+        }
+        else{
+            toggleClock();
+        }        
+    }
 }
