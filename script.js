b9931d87c9aa5d2e893cf848a4dd7b74a3d6a76a
xroll <xroll.gothub@gmail.com>
2020-09-26 22:43:27 +0200

    (Feat) Clock display - Add display for both players - Timer initial state - Format time

--- script.js
+++ script.js
@@ -5,14 +5,6 @@ const instructions = document.getElementById('instructions');
 const timerStep = document.getElementById('timer-step');
 const chessClock = document.getElementById('chess-clock');
 
-const steps = [infos, timerStep, chessClock ];
-
-//Default-time
-
-const timerDisplay = document.getElementById('timer-display');
-
-
-
 //General functions
 
 function show(element) {
@@ -24,6 +16,8 @@ function hide(element) {
     element.classList.add("hide");
     element.classList.remove("show");
 }
+
+//Todo : Refactor functions into switch cases
 
 function showTimerStep(){
     hide(infos);
@@ -50,12 +44,80 @@ function back(){
     show(infos);
 }
 
-function setTimer()
-{
-    console.log(document.getElementById('btn10').value)
+//Timer
+
+let timer;
+
+function timerSelect(){
+    hide(chessClock);
+    show(timerStep);
+    
 }
 
 
 
+function setTimer(value)
+{
+    timer = 0;
+    timer = value * 60;
+    hide(timerStep);
+    initialState(timer);
+    show(chessClock);
+    
+}
 
+//Chess clock
 
+let whiteTimeLeft;
+let blackTimeLeft;
+let isWhiteActive = false;
+let isBlackActive = false;
+
+function initialState(timer){
+
+    let initialHoursLeft = 0;
+    let initialMinutesLeft = 0;
+    let initialSecondsLeft = 0;
+    
+    //Initial time
+    initialHoursLeft = Math.floor(timer / 3600) % 24;
+    initialMinutesLeft = Math.floor(timer / 60) % 60;
+    initialSecondsLeft = timer % 60;
+
+    //Display
+    
+    //Hours
+    console.log(initialHoursLeft);
+    if(initialHoursLeft > 0) {
+        document.getElementById('whiteHours').style.display = "block";
+        document.getElementById('blackHours').style.display = "block";
+        document.getElementById('whiteHours').innerHTML = initialHoursLeft + ' :&nbsp;';
+        document.getElementById('blackHours').innerHTML = initialHoursLeft + " :&nbsp;";
+    }
+    else {        
+        document.getElementById('whiteHours').style.display = "none";
+        document.getElementById('blackHours').style.display = "none";
+    }    
+    //Minutes    
+    document.getElementById('whiteMinutes').innerHTML = formatTime(initialMinutesLeft) + " :&nbsp;";
+    document.getElementById('blackMinutes').innerHTML = formatTime(initialMinutesLeft) + " :&nbsp;";
+    //Seconds
+    document.getElementById('whiteSeconds').innerHTML = formatTime(initialSecondsLeft);    
+    document.getElementById('blackSeconds').innerHTML = formatTime(initialSecondsLeft);
+
+    //Set initial time for players
+
+    whiteTimeLeft = timer;
+    blackTimeLeft = timer;
+
+    //Clock status
+
+    isClockActive = false;
+    isWhiteActive = false;
+    isBlackActive = false;
+}
+
+//Format time
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
